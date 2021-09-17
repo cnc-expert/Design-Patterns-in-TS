be665f93a6efeeeea2cc952775a7b6c1b4920dc3
Aleksandr Loshkin <cnc-expert@mail.ru>
2021-09-17 22:05:03 +0500

    Behavioral design pattern "Observer" 	new file:   behavior_observer.ts

---
+++ behavior_observer.ts
@@ -0,0 +1,39 @@
+// Behavioral design pattern "Observer"
+
+class Subscriber {
+    name: string;
+    
+    constructor(name = "Anonymous") {
+        this.name = name;
+    }
+
+    inform(aboutNews: string) {
+        console.log(`${this.name} knows about "${aboutNews}"`);
+    };
+}
+
+class News {
+    subscribers: Subscriber[] = [];
+
+    publishNews(text: string) {
+        this.subscribers.forEach(s => s.inform(text));
+    }
+
+    subscribe(s: Subscriber) {
+        this.subscribers.push(s);
+    }
+
+    unsubscribe(s: Subscriber) {
+        this.subscribers = this.subscribers.filter(subs => subs != s);
+    }
+}
+
+console.clear();
+const Max = new Subscriber("Max");
+const Anonym = new Subscriber();
+const news = new News();
+news.subscribe(Max);
+news.subscribe(Anonym);
+news.publishNews("Web is fun");
+news.unsubscribe(Max);
+news.publishNews("There is too much info");
