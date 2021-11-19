b17f75ec3c633a9e40a0024db2736bdfe22d66b0
Aleksandr Loshkin <cnc-expert@mail.ru>
2021-11-19 08:47:55 +0500

    Structural design pattern "Flyweight". Directory reorganized. 	renamed:    design_patterns/behavioral/command.ts -> behavioral/command.ts 	renamed:    design_patterns/behavioral/iterator.ts -> behavioral/iterator.ts 	renamed:    design_patterns/behavioral/mediator.ts -> behavioral/mediator.ts 	renamed:    design_patterns/behavioral/mediator_with_observer.ts -> behavioral/mediator_with_observer.ts 	renamed:    design_patterns/behavioral/memento.ts -> behavioral/memento.ts 	renamed:    design_patterns/behavioral/observer.ts -> behavioral/observer.ts 	renamed:    design_patterns/behavioral/responsibility_chain.ts -> behavioral/responsibility_chain.ts 	renamed:    design_patterns/behavioral/state.ts -> behavioral/state.ts 	renamed:    design_patterns/behavioral/strategy.ts -> behavioral/strategy.ts 	renamed:    design_patterns/behavioral/template.ts -> behavioral/template.ts 	renamed:    design_patterns/behavioral/visitor.ts -> behavioral/visitor.ts 	new file:   structural/flyweight.ts

---
+++ structural/flyweight.ts
@@ -0,0 +1,36 @@
+// Structural design pattern "Flyweight"
+//
+// A classic example are the data structures used representing characters
+// in a word processor. Naively, each character in a document might have
+// a glyph object containing its font outline, font metrics, and other
+// formatting data. However, this would use hundreds or thousands of bytes
+// of memory for each character. Instead, each character can have a reference
+// to a glyph object shared by every instance of the same character
+// in the document.
+
+class CarData {
+	constructor(public model: string) {};
+}
+
+class CarFactory {
+	private cars: { [model: string]: CarData } = {};
+
+	create(model: string) {
+		if (!this.cars[model]) this.cars[model] = new CarData(model);
+		return this.cars[model];
+	}
+
+	getCarsJson() {
+		return JSON.stringify(this.cars);
+	}
+}
+
+// ----- Usage -----
+console.clear();
+
+const factory = new CarFactory();
+const tazik = factory.create("vaz");
+const bum = factory.create("bumer");
+const tazz = factory.create("vaz");
+console.log(tazz == tazik);
+console.log(factory.getCarsJson());
