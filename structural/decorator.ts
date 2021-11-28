d3f3710e9c95aee55f2d2f4276f207dfb70bf6f3
Aleksandr Loshkin <cnc-expert@mail.ru>
2021-11-28 20:48:28 +0500

    Difference between Proxy and Decorator. 	modified:   structural/decorator.ts

--- structural/decorator.ts
+++ structural/decorator.ts
@@ -4,6 +4,12 @@
 // without affecting the behavior of other objects from the same class.
 // The decorator pattern is often useful for adhering to the Single Responsibility Principle, as it allows functionality to be divided between classes with unique areas of concern.[2] Decorator use can be more efficient than subclassing, because an object's behavior can be augmented without defining an entirely new object.
 
+// Difference between Decorator and Proxy:
+// * Decorator Pattern focuses on dynamically adding functions to an object,
+//   Proxy Pattern focuses on controlling access to an object.
+// * A Decorator is always passed its delegatee.
+//   A Proxy might create it himself, or he might have it injected.
+
 interface ITypicalCar {
 	getPrice(): number;
 	getDescription(): string;
